79326a1a422ea2602a878a4a8c409f6ab76ace5e
Megan <meganannetteolson@yahoo.com>
2017-12-04 14:03:25 -0800

    Validate presence and relationships in product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   has_many :order_items
   has_many :reviews
+  validates_presence_of :name, :price, :on_sale, :stock
   scope :on_sale, -> { where("on_sale = ?", (true))}
 end
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+describe Product do
+  it { should have_many :reviews }
+  it { should have_many :order_items }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :on_sale }
+  it { should validate_presence_of :stock }
+end
