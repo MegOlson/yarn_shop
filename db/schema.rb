48f1d4e180caac27cfefa8b9fadf8aa51ad23f5e
Megan <meganannetteolson@yahoo.com>
2017-12-04 11:47:52 -0800

    Add save for later button and low stock notification.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204191144) do
+ActiveRecord::Schema.define(version: 20171204193914) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20171204191144) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "stock"
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Product.destroy_all
 
 50.times do |index|
-  product = Product.create!(name: Faker::Commerce.color, price: Faker::Commerce.price)
+  product = Product.create!(name: Faker::Commerce.color, price: Faker::Commerce.price, stock: Faker::Number.between(1, 75))
 end
 
 p "Created #{Product.count} products"
