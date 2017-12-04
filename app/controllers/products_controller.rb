71b33c5ab3910d3bf6e22eec1b8f7668f1cf6136
Megan <meganannetteolson@yahoo.com>
2017-12-04 09:14:14 -0800

    add 'add to cart' form on product index page.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,11 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all
+    @order_item = current_order.order_items.new
   end
-  
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
 end
