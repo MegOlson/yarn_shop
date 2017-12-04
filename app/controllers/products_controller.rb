c655e8321ea759ebe3f7d2a4643da2a1326a7383
Megan <meganannetteolson@yahoo.com>
2017-12-04 13:03:41 -0800

    Display products on sale.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,7 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
     @order_item = current_order.order_items.new
+    @discount_products = Product.on_sale
   end
 
   def show
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
   has_many :order_items
   has_many :reviews
+  scope :on_sale, -> { where("on_sale = ?", (true))}
 end
