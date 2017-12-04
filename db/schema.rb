889a16d6b7f4c89a8f62608baa4504fbb4f177cd
Megan <meganannetteolson@yahoo.com>
2017-12-04 13:16:27 -0800

    Add sale price.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171204205245) do
+ActiveRecord::Schema.define(version: 20171204211213) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20171204205245) do
     t.datetime "updated_at",                 null: false
     t.integer  "stock"
     t.boolean  "on_sale",    default: false
+    t.decimal  "sale_price"
   end
 
   create_table "reviews", force: :cascade do |t|
