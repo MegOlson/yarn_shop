c6b9a3ffde0749457f27747c7c49b63a64ab249b
Megan <meganannetteolson@yahoo.com>
2017-12-05 09:34:15 -0800

    Fix stripe and process order with test.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,5 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_test_qvgd3JYD9jF8jv7u81CgSHWj'],
-  :secret_key      => ENV['sk_test_7wHwoEGnDKSW2Z3x41RjLvXM']
+  :publishable_key => 'pk_test_qvgd3JYD9jF8jv7u81CgSHWj',
+  :secret_key      => 'sk_test_7wHwoEGnDKSW2Z3x41RjLvXM'
 }
-
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
