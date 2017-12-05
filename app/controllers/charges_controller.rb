23e533b935bdd951b8db48ad3f935b5e93eb5a0d
Megan <meganannetteolson@yahoo.com>
2017-12-05 11:00:52 -0800

    Fix order processing price/

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,7 +4,7 @@ class ChargesController < ApplicationController
 
   def create
     # Amount in cents
-    @amount = (current_order.total_price)
+    @amount = ((current_order.total_price) * 100).to_i
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
