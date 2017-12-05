beb81c3ad91ae0886fd040f6b5bec96395b4b1b8
Megan <meganannetteolson@yahoo.com>
2017-12-04 16:32:16 -0800

    Final Monday commit.

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -2,6 +2,4 @@ require "rails_helper"
 
 describe Order do
   it { should have_many :order_items }
-  it { should validate_presence_of :status }
-  it { should validate_presence_of :total_price }
 end
